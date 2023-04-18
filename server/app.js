f7be4df729e3d1d841badcc2a31572fbf9baa85e
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-04-18 17:29:27 +0300

    add JSON handler and post a/artist route handler

--- server/app.js
+++ server/app.js
@@ -25,6 +25,21 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log('Body: ', req.body)
+  next()
+})
+
+app.get('/artists', (req, res) => {
+  const allArtists = getAllArtists()
+  res.send(allArtists)
+})
+
+app.post('/artists', (req, res) => {
+  const newData = addArtist(req.body)
+  res.send(newData)
+})
 
 // DO NOT MODIFY
 if (require.main === module) {
